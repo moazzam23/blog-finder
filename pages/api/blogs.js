fe349741ff6b8ddb26a797c2425b8dbfcea83985
Moazzam <baigm9033@gmail.com>
2023-09-13 16:56:18 +0500

    update

--- pages/api/blogs.js
+++ pages/api/blogs.js
@@ -4,7 +4,6 @@
 import * as fs from 'fs';
 
 export default async function handler(req, res) {
-
   let data=await fs.promises.readdir("Blogdata")
   let myblog;
   let Allblogs=[];
@@ -12,33 +11,12 @@ export default async function handler(req, res) {
   const item = data[index];
   myblog= await fs.promises.readFile(('blogdata/' + item),'utf-8')
   Allblogs.push(JSON.parse(myblog))
-  
-
-
-// let data= await fs.promises.readdir("Blogdata");
-// let myblog;
-// let allblogs =[];
-// for (let index = 0; index < data.lenght ; index++) {
-//   const item = data[index];
-//   myblog= await fs.promises.readFile(('Blogdata/' + item),'utf-8')
-//  allblogs.push(JSON.parse(myblog))
-// }
-res.status(200).json(Allblogs)   
-}
-
-// export default function handler(req, res) {
-
-//   fs.readdir("blogs",(err,data)=>{
-//     console.log('')
-//     res.status(200).json(data)   
-//   })
-// import * as fs from 'fs';
+  }
+  res.status(200).json(Allblogs)   
 }
  export default function anotherhandler(req, res) {
-
    fs.readFile("blogs/javascript.json","utf-8",(err,data)=>{
      console.log('')
      res.status(200).json(JSON.parse(data))   
    })
- 
  }
